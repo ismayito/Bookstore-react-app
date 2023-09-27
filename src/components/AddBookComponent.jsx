4f46573f2fed0a970152b09c7c54d3c1a1c5ace3
Ismail mayito <ismayito1@gmail.com>
2023-09-27 10:45:28 +0300

    Fix review changes

--- src/components/AddBookComponent.jsx
+++ src/components/AddBookComponent.jsx
@@ -4,19 +4,22 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { addBooks } from './redux/books/booksSlice';
 
-const AddBookComponent = ({ input, inputFunc }) => {
+const AddBookComponent = ({
+  inputTitle, inputTitleFunc, inputAuthor, inputAuthorFunc,
+}) => {
   const dispatch = useDispatch();
   const currentDate = new Date();
   const itemId = currentDate.getTime();
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(addBooks({
-      title: input,
-      author: 'Mayito Ismail',
+      title: inputTitle,
+      author: inputAuthor,
       item_id: itemId,
       category: 'fiction',
     }));
-    inputFunc('');
+    inputTitleFunc('');
+    inputAuthorFunc('');
   };
   return (
     <div>
@@ -26,8 +29,10 @@ const AddBookComponent = ({ input, inputFunc }) => {
 };
 
 AddBookComponent.propTypes = {
-  input: PropTypes.string.isRequired,
-  inputFunc: PropTypes.func.isRequired,
+  inputTitle: PropTypes.string.isRequired,
+  inputTitleFunc: PropTypes.func.isRequired,
+  inputAuthor: PropTypes.string.isRequired,
+  inputAuthorFunc: PropTypes.func.isRequired,
 };
 
 export default AddBookComponent;
--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -4,21 +4,28 @@ import AddBookComponent from './AddBookComponent';
 
 const FormComponent = () => {
   const [input, setInput] = useState('');
+  const [author, setAuthor] = useState('');
   const handleInput = (e) => {
     setInput(e.target.value);
   };
+
+  const handleAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+
   return (
     <>
       <div className="formContainer">
         <form>
           <input type="text" placeholder="Book title" value={input} onChange={handleInput} className="bookTitle" />
+          <input type="text" placeholder="author" value={author} onChange={handleAuthor} className="bookTitle" />
           <select name="books" id="books" className="selectCategory">
             <option value="category">category</option>
             <option value="fiction">fiction</option>
             <option value="adventure">adventure</option>
             <option value="entertainment">entertainment</option>
           </select>
-          <AddBookComponent input={input} inputFunc={setInput} className="submitButton" />
+          <AddBookComponent inputTitle={input} inputAuthor={author} inputTitleFunc={setInput} inputAuthorFunc={setAuthor} className="submitButton" />
         </form>
       </div>
 
